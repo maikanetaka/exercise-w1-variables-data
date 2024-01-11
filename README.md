3227dd4bf1c2496a6b4926800a00c9423e19ba6f
AntonellaMorittu <antonellamorittu@Antonellas-MBP.(none)>
2024-01-11 11:06:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+<h1 align="center">
+  <a href="">
+    <img src="src/assets/week1.svg" alt="Boiler Plate">
+  </a>
+</h1>
+
+# JavaScript Variables & Datatypes Exercise
+
+In this week’s team session, you will practice JavaScript basics and work on applying the concepts we have learned throughout the week. This will be done during the scheduled team session, you decide in the team if you do it in small groups or pairs.
+
+### Instructions:
+
+Take a look into the index.js file and follow the instructions to complete the exercises 1-7 (n 8 is a bonus exercise).
+
+### Hint:
+
+Don't use `var`, stick to `let` and `const`.
+
+## Getting Started with the Project
+
+Fork this repository and start working in your local environment (you can download LiveServer to run your project locally).
---
+++ src/index.js
@@ -0,0 +1,60 @@
+/*
+1) (This has a DEMO solution.)
+Create a variable that stores the name of your cafe.
+Print out a greeting such as "Welcome to Technigo Cafe! What would you like to order today?"
+Where echnigo Cafe is replaced by the name of your cafe that is stored in the variable.
+*/
+const cafeName = "Technigo Cafe";
+console.log(`Welcome to ${cafeName}! What would you like to order today?`);
+
+/*
+2)
+Create a variable that stores the price of a coffee.
+Create a variable that stores how many coffees the customer wants.
+Print out the total price such as "There you go, that'll be 10 euros"
+Where 10 is replaced by the calculation of the total price.
+*/
+
+/*
+3)
+Create a variable that stores a boolean.
+Print out "You said this coffee is the best, that was actually true"
+Where true is replaced by your varible
+*/
+
+/*
+4)
+Create a variable called cafeGuests, that shows us how many cafeGuests we have.
+Assign it a value.
+Print it out.
+assign it a new value.
+Print it out. => This should give you the new value.
+*/
+
+/*
+5)
+Create a variable called maxGuests, that shows us how many guests are allowed in the cafe.
+Assign it a value.
+Print it out.
+assign it a new value.
+Print it out. => This should give you an error because it shouldn't be able to be changed.
+*/
+
+/*
+6)
+Create a variable that stores a string.
+Print out that string in only UPPERCASE letters.
+*/
+
+/*
+7)
+Print out the same string in only lowercase letters.
+*/
+
+/*
+8) **BONUS**
+Print out the string "Today we have a special summer deal!". 
+Then figure out a way to replace the word "summer" in the string with the word "winter"
+Should give you => "Today we have a special winter deal!"
+(check for a specific string method...)
+*/
